f838dad55d5abf60d62b21a7e20a3233210ffdc2
ch4xb7sspq-eng <ch4xb7sspq@privaterelay.appleid.com>
2025-10-16 15:40:35 +0900

    更新

--- daisho-day1/script.js
+++ daisho-day1/script.js
@@ -4,10 +4,10 @@ const message=document.getElementByld("message");
 helloBtn.addEventListener("click",()=>{
     const hour=new Date().getHours();
     let greeting="こんにちは！";
-    if (hour<12){
+    if(hour<12){
         greeting="おはようございます！";
-    }else if (hour>=18){
-        greeting="こんばんは！"
+    }else if(hour>=18){
+        greeting="こんばんは！";
     }
-    message.textContent='${greeting}GitHub Pages の公開に成功しました！';
+    message.textContent='${greeting} GitHub Pages の公開に成功しました！';
 });
